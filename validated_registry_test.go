bd3eb10ec41ee64ccc27f20365f803d27d34e199
Lukasz Lato <latolukasz@gmail.com>
2021-07-24 15:15:49 +0200

    better code coverage

--- validated_registry_test.go
+++ validated_registry_test.go
@@ -21,6 +21,8 @@ type validatedRegistryNotRegisteredEntity struct {
 func TestValidatedRegistry(t *testing.T) {
 	registry := &Registry{}
 	registry.RegisterMySQLPool("root:root@tcp(localhost:3311)/test")
+	registry.RegisterLocalCache(100)
+	registry.RegisterLocalCache(50, "another")
 	registry.RegisterEnum("enum_map", []string{"a", "b"}, "b")
 	entity := &validatedRegistryEntity{}
 	registry.RegisterEntity(entity)
@@ -48,6 +50,23 @@ func TestValidatedRegistry(t *testing.T) {
 	assert.Equal(t, "a", enum.GetDefault())
 	assert.True(t, enum.Has("a"))
 	assert.False(t, enum.Has("c"))
+
+	mysqlPools := validated.GetMySQLPools()
+	assert.Len(t, mysqlPools, 1)
+	assert.NotNil(t, mysqlPools["default"])
+	assert.Equal(t, "default", mysqlPools["default"].GetCode())
+	assert.Equal(t, "test", mysqlPools["default"].GetDatabase())
+	assert.Equal(t, 5, mysqlPools["default"].GetVersion())
+	assert.Equal(t, "root:root@tcp(localhost:3311)/test?multiStatements=true", mysqlPools["default"].GetDataSourceURI())
+
+	localCachePools := validated.GetLocalCachePools()
+	assert.Len(t, localCachePools, 2)
+	assert.NotNil(t, localCachePools["default"])
+	assert.NotNil(t, localCachePools["another"])
+	assert.Equal(t, "default", localCachePools["default"].GetCode())
+	assert.Equal(t, 100, localCachePools["default"].GetLimit())
+	assert.Equal(t, "another", localCachePools["another"].GetCode())
+	assert.Equal(t, 50, localCachePools["another"].GetLimit())
 
 	assert.PanicsWithError(t, "entity 'beeorm.validatedRegistryNotRegisteredEntity' is not registered", func() {
 		validated.GetTableSchemaForEntity(&validatedRegistryNotRegisteredEntity{})
