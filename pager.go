7fd3ff3fcc529cf82d06277c2a659ca3257eb0c6
lukaszlato <latolukasz@gmail.com>
2022-12-13 23:12:56 +0100

    Add comments and improve Pager type
    This commit includes the following changes:
    
    - Added comments to explain the purpose of the `Pager` type and the meaning of its fields and methods.
    - Renamed the fields and methods to use camel case, as is idiomatic in Go.
    - Added a `NewPager()` function to make it easier to create a new `Pager` instance.

--- pager.go
+++ pager.go
@@ -1,10 +1,15 @@
 package beeorm
 
+// Pager represents a paginated list of results.
 type Pager struct {
+	// CurrentPage is the current page number.
 	CurrentPage int
-	PageSize    int
+
+	// PageSize is the number of items per page.
+	PageSize int
 }
 
+// NewPager creates a new Pager with the given page number and page size.
 func NewPager(currentPage, pageSize int) *Pager {
 	return &Pager{
 		CurrentPage: currentPage,
@@ -12,14 +17,17 @@ func NewPager(currentPage, pageSize int) *Pager {
 	}
 }
 
+// GetPageSize returns the page size of the Pager.
 func (pager *Pager) GetPageSize() int {
 	return pager.PageSize
 }
 
+// GetCurrentPage returns the current page number of the Pager.
 func (pager *Pager) GetCurrentPage() int {
 	return pager.CurrentPage
 }
 
+// IncrementPage increments the current page number of the Pager.
 func (pager *Pager) IncrementPage() {
 	pager.CurrentPage++
 }
