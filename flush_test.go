011a1b94d0dc967237a09120d7aa1c13e0d3c072
lukaszlato <latolukasz@gmail.com>
2023-09-15 08:59:12 +0200

    fixed FakeDelete in Cache Query

--- flush_test.go
+++ flush_test.go
@@ -188,7 +188,7 @@ func testFlush(t *testing.T, local bool, redis bool) {
 	newEntity.BoolNullable = &boolNullable
 	floatNullable := 12.23
 	newEntity.FloatNullable = &floatNullable
-	float32Nullable := float32(12.23)
+	float32Nullable := float32(12.24)
 	newEntity.Float32Nullable = &float32Nullable
 	newEntity.SetNullable = testSet{testEnumDefinition.B, testEnumDefinition.C}
 	newEntity.SetNotNull = testSet{testEnumDefinition.A, testEnumDefinition.C}
@@ -197,8 +197,8 @@ func testFlush(t *testing.T, local bool, redis bool) {
 	newEntity.Blob = []byte("test binary")
 	newEntity.Bool = true
 	newEntity.Float64 = 986.2322
-	newEntity.Decimal = 78.2357
-	decimalNullable := 123.2320932
+	newEntity.Decimal = 78.24
+	decimalNullable := 123.23
 	newEntity.DecimalNullable = &decimalNullable
 	newEntity.Float64Default = 8932.299423
 	newEntity.Float64Signed = -352.120321
@@ -240,41 +240,41 @@ func testFlush(t *testing.T, local bool, redis bool) {
 	assert.Equal(t, "Test name", entity.Name)
 	assert.Equal(t, -19, entity.Age)
 	assert.Equal(t, uint(134), entity.Uint)
-	assert.Nil(t, entity.UintNullable)
-	assert.Nil(t, entity.IntNullable)
-	assert.Equal(t, uint16(0), entity.Year)
-	assert.Nil(t, entity.YearNullable)
-	assert.Nil(t, entity.BoolNullable)
-	assert.Nil(t, entity.FloatNullable)
-	assert.Nil(t, entity.Float32Nullable)
-	assert.Nil(t, entity.SetNullable)
+	assert.Equal(t, uint(23), *entity.UintNullable)
+	assert.Equal(t, -45, *entity.IntNullable)
+	assert.Equal(t, uint16(2023), entity.Year)
+	assert.Equal(t, uint16(2025), *entity.YearNullable)
+	assert.True(t, *entity.BoolNullable)
+	assert.Equal(t, 12.23, *entity.FloatNullable)
+	assert.Equal(t, float32(12.24), *entity.Float32Nullable)
+	assert.Equal(t, testSet{testEnumDefinition.B, testEnumDefinition.C}, entity.SetNullable)
 	assert.Equal(t, testSet{testEnumDefinition.A, testEnumDefinition.C}, entity.SetNotNull)
-	assert.Equal(t, testEnum(""), entity.EnumNullable)
-	assert.Equal(t, testEnumDefinition.B, entity.EnumNotNull)
-	assert.Nil(t, entity.Blob)
-	assert.False(t, entity.Bool)
-	assert.Equal(t, 0.0, entity.Float64)
-	assert.Equal(t, 0.0, entity.Decimal)
-	assert.Nil(t, entity.DecimalNullable)
-	assert.Equal(t, 0.0, entity.Float64Default)
-	assert.Equal(t, 0.0, entity.Float64Signed)
-	assert.Equal(t, new(time.Time).UTC(), entity.Time)
+	assert.Equal(t, testEnumDefinition.C, entity.EnumNullable)
+	assert.Equal(t, testEnumDefinition.A, entity.EnumNotNull)
+	assert.Equal(t, []byte("test binary"), entity.Blob)
+	assert.True(t, entity.Bool)
+	assert.Equal(t, 986.2322, entity.Float64)
+	assert.Equal(t, 78.24, entity.Decimal)
+	assert.Equal(t, 123.23, *entity.DecimalNullable)
+	assert.Equal(t, 8932.299423, entity.Float64Default)
+	assert.Equal(t, -352.120321, entity.Float64Signed)
+	assert.Equal(t, time.Date(2023, 11, 12, 22, 12, 34, 0, time.UTC), entity.Time)
 	assert.Equal(t, new(time.Time).UTC(), entity.TimeWithTime)
-	assert.Nil(t, entity.TimeNullable)
-	assert.Nil(t, entity.TimeWithTimeNullable)
-	assert.Equal(t, "", entity.FlushStruct.Name2)
-	assert.Equal(t, 0, entity.FlushStruct.Age)
-	assert.Equal(t, "", entity.FlushStruct.Sub.Name3)
-	assert.Equal(t, 0, entity.FlushStruct.Sub.Age3)
-	assert.Nil(t, entity.FlushStruct.TestTime)
-	assert.Nil(t, entity.Int8Nullable)
-	assert.Nil(t, entity.Int16Nullable)
-	assert.Nil(t, entity.Int32Nullable)
-	assert.Nil(t, entity.Int64Nullable)
-	assert.Nil(t, entity.Uint8Nullable)
-	assert.Nil(t, entity.Uint16Nullable)
-	assert.Nil(t, entity.Uint32Nullable)
-	assert.Nil(t, entity.Uint64Nullable)
-	assert.Equal(t, "", entity.SubName)
-	assert.Equal(t, float32(0), entity.SubAge)
+	assert.Equal(t, time.Date(2023, 8, 16, 12, 23, 11, 0, time.UTC), *entity.TimeNullable)
+	assert.Equal(t, time.Date(2025, 11, 4, 21, 0, 5, 6, time.UTC), *entity.TimeWithTimeNullable)
+	assert.Equal(t, "Tom", entity.FlushStruct.Name2)
+	assert.Equal(t, 23, entity.FlushStruct.Age)
+	assert.Equal(t, "Zoya", entity.FlushStruct.Sub.Name3)
+	assert.Equal(t, 18, entity.FlushStruct.Sub.Age3)
+	assert.Equal(t, time.Date(1982, 11, 4, 21, 0, 5, 6, time.UTC), *entity.FlushStruct.TestTime)
+	assert.Equal(t, int8(23), *entity.Int8Nullable)
+	assert.Equal(t, int16(-29), *entity.Int16Nullable)
+	assert.Equal(t, int32(-2923), *entity.Int32Nullable)
+	assert.Equal(t, int64(98872), *entity.Int64Nullable)
+	assert.Equal(t, uint8(23), *entity.Uint8Nullable)
+	assert.Equal(t, uint16(29), *entity.Uint16Nullable)
+	assert.Equal(t, uint32(2923), *entity.Uint32Nullable)
+	assert.Equal(t, uint64(98872), *entity.Uint64Nullable)
+	assert.Equal(t, "sub name", entity.SubName)
+	assert.Equal(t, float32(123), entity.SubAge)
 }
