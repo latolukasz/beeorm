7a2d35d292547f827a39410614bf08fdfe59ca0a
lukaszlato <latolukasz@gmail.com>
2023-10-04 10:35:58 +0200

    Modified column

--- get_by_id_benchmarks_test.go
+++ get_by_id_benchmarks_test.go
@@ -1,47 +1,42 @@
 package beeorm
 
-//
-//import (
-//	"testing"
-//)
-//
-//type getByIDBenchmarkEntity struct {
-//	ORM     `orm:"localCache;redisCache"`
-//	ID      uint
-//	Name    string
-//	Int     int
-//	Bool    bool
-//	Float   float64
-//	Decimal float32 `orm:"decimal=10,2"`
-//}
-//
-//// BenchmarkGetByIDLocalCache-10    	63629875	        18.75 ns/op	       0 B/op	       0 allocs/op
-//func BenchmarkGetByIDLocalCache(b *testing.B) {
-//	benchmarkGetByIDCache(b, true, false)
-//}
-//
-//// BenchmarkGetByIDRedisCache-10    	    1966	    590329 ns/op	     272 B/op	      10 allocs/op
-//func BenchmarkGetByIDRedisCache(b *testing.B) {
-//	benchmarkGetByIDCache(b, false, true)
-//}
-//
-//func benchmarkGetByIDCache(b *testing.B, local, redis bool) {
-//	var entity *getByIDBenchmarkEntity
-//	registry := &Registry{}
-//	registry.RegisterLocalCache(10000)
-//	c := PrepareTables(nil, registry, 5, 6, "", entity)
-//	schema := GetEntitySchema[*loadByIDBenchmarkEntity](c)
-//	schema.DisableCache(!local, !redis)
-//	entity = &loadByIDBenchmarkEntity{}
-//	entity.Name = "Name"
-//	entity.Int = 1
-//	entity.Float = 1.3
-//	entity.Decimal = 12.23
-//	c.Flusher().Track(entity).Flush()
-//	GetByID[*loadByIDBenchmarkEntity](c, 1)
-//	b.ResetTimer()
-//	b.ReportAllocs()
-//	for n := 0; n < b.N; n++ {
-//		GetByID[*loadByIDBenchmarkEntity](c, 1)
-//	}
-//}
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+type getByIDBenchmarkEntity struct {
+	ID   uint64 `orm:"localCache;redisCache"`
+	Name string
+}
+
+// BenchmarkGetByIDLocalCache-10    	66659722	        17.62 ns/op	       0 B/op	       0 allocs/op
+func BenchmarkGetByIDLocalCache(b *testing.B) {
+	benchmarkGetByIDCache(b, true, false)
+}
+
+// BenchmarkGetByIDRedisCache-10    	    1966	    590329 ns/op	     272 B/op	      10 allocs/op
+func BenchmarkGetByIDRedisCache(b *testing.B) {
+	benchmarkGetByIDCache(b, false, true)
+}
+
+func benchmarkGetByIDCache(b *testing.B, local, redis bool) {
+	var entity *getByIDBenchmarkEntity
+	registry := &Registry{}
+	registry.RegisterLocalCache(10000)
+	c := PrepareTables(nil, registry, entity)
+	schema := GetEntitySchema[getByIDBenchmarkEntity](c)
+	schema.DisableCache(!local, !redis)
+
+	entity = NewEntity[getByIDBenchmarkEntity](c).TrackedEntity()
+	entity.Name = "Name"
+	err := c.Flush(false)
+	assert.NoError(b, err)
+
+	GetByID[getByIDBenchmarkEntity](c, entity.ID)
+	b.ResetTimer()
+	b.ReportAllocs()
+	for n := 0; n < b.N; n++ {
+		GetByID[getByIDBenchmarkEntity](c, entity.ID)
+	}
+}
