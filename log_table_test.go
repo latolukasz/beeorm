1b4bd0fee16a2d7c567b582f958a0a6cb40d38e3
lukaszlato <latolukasz@gmail.com>
2023-10-19 16:15:09 +0200

    bigger cache prefix

--- log_table_test.go
+++ log_table_test.go
@@ -23,7 +23,6 @@ func TestLogTable(t *testing.T) {
 	entity.Age = 18
 	err := c.Flush(false)
 	assert.NoError(t, err)
-
 	err = ConsumeLazyFlushEvents(c, false)
 	assert.Nil(t, err)
 
@@ -40,4 +39,25 @@ func TestLogTable(t *testing.T) {
 	assert.Equal(t, strconv.FormatUint(entity.ID, 10), bind["ID"])
 	assert.Equal(t, "Test", bind["Name"])
 	assert.Equal(t, "18", bind["Age"])
+
+	c.SetMetaData("source", "test case")
+	entity = NewEntity[logTableEntity](c).TrackedEntity()
+	entity.Name = "Test 2"
+	entity.Age = 30
+	err = c.Flush(false)
+	assert.NoError(t, err)
+	err = ConsumeLazyFlushEvents(c, false)
+	assert.Nil(t, err)
+
+	logs = Search[LogEntity[logTableEntity]](c, NewWhere("1"), nil)
+	assert.Len(t, logs, 2)
+	assert.Equal(t, entity.ID, logs[1].EntityID)
+	assert.NotNil(t, logs[1].Meta)
+	assert.Nil(t, logs[1].After)
+	assert.NotNil(t, logs[1].Before)
+	bind = nil
+	err = jsoniter.ConfigFastest.Unmarshal(logs[1].Meta, &bind)
+	assert.NoError(t, err)
+	assert.Len(t, bind, 1)
+	assert.Equal(t, "test case", bind["source"])
 }
