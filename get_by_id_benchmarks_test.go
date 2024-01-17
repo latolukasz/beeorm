662b86006f4bd1c3dc8dfd1637af4877b4a3ab08
lukaszlato <latolukasz@gmail.com>
2024-01-17 09:21:40 +0100

    disabled archived table for logs

--- get_by_id_benchmarks_test.go
+++ get_by_id_benchmarks_test.go
@@ -11,20 +11,29 @@ type getByIDBenchmarkEntity struct {
 	Name string
 }
 
-// BenchmarkGetByIDLocalCache-10    	66659722	        17.62 ns/op	       0 B/op	       0 allocs/op
-func BenchmarkGetByIDLocalCache(b *testing.B) {
+type getByIDBenchmarkEntityLimit struct {
+	ID   uint64 `orm:"localCache=10"`
+	Name string
+}
+
+// BenchmarkGetByIDLocalCache-10    	62160598	        19.32 ns/op	       0 B/op	       0 allocs/op
+func BenchmarkGetByIDLocalCacheNoLimit(b *testing.B) {
 	benchmarkGetByIDCache(b, true, false)
 }
 
+// BenchmarkGetByIDLocalCacheLimit-10    	42071001	        28.35 ns/op	       0 B/op	       0 allocs/op
+func BenchmarkGetByIDLocalCacheLimit(b *testing.B) {
+	benchmarkGetByIDLocalCacheLimit(b)
+}
+
 // BenchmarkGetByIDRedisCache-10    	    1966	    590329 ns/op	     272 B/op	      10 allocs/op
-func BenchmarkGetByIDRedisCache(b *testing.B) {
+func BenchmarkGetByIDRedisCacheNoLimit(b *testing.B) {
 	benchmarkGetByIDCache(b, false, true)
 }
 
 func benchmarkGetByIDCache(b *testing.B, local, redis bool) {
 	var entity *getByIDBenchmarkEntity
 	registry := NewRegistry()
-	registry.RegisterLocalCache(DefaultPoolCode, 0)
 	orm := PrepareTables(nil, registry, entity)
 	schema := GetEntitySchema[getByIDBenchmarkEntity](orm)
 	schema.DisableCache(!local, !redis)
@@ -39,5 +48,23 @@ func benchmarkGetByIDCache(b *testing.B, local, redis bool) {
 	b.ReportAllocs()
 	for n := 0; n < b.N; n++ {
 		GetByID[getByIDBenchmarkEntity](orm, entity.ID)
+	}
+}
+
+func benchmarkGetByIDLocalCacheLimit(b *testing.B) {
+	var entity *getByIDBenchmarkEntityLimit
+	registry := NewRegistry()
+	orm := PrepareTables(nil, registry, entity)
+
+	entity = NewEntity[getByIDBenchmarkEntityLimit](orm)
+	entity.Name = "Name"
+	err := orm.Flush()
+	assert.NoError(b, err)
+
+	GetByID[getByIDBenchmarkEntityLimit](orm, entity.ID)
+	b.ResetTimer()
+	b.ReportAllocs()
+	for n := 0; n < b.N; n++ {
+		GetByID[getByIDBenchmarkEntityLimit](orm, entity.ID)
 	}
 }
